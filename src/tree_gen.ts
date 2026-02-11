5d558f847c918d7b9f1d9b2f69941e6e2ff468cc
Loxie <gidropony1905@gmail.com>
2026-02-11 13:32:43 +0400

    added tree gen and tests

---
+++ src/tree_gen.ts
@@ -0,0 +1,104 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+askingpath()
+
+
+function askingpath(){
+    rl.question('Please enter the path for the file (default: current directory): ', (answer) => {
+        if(answer == 'exit'){
+            rl.close();
+            return;
+        }
+        let filepath = answer || 'tree.txt';
+        let dir = path.dirname(filepath);
+        
+        if (answer == '.' || answer == '..') {
+            filepath = filepath + '/tree.txt'
+            askingheight(filepath);
+        }
+        if(fs.existsSync(dir)){
+            askingheight(filepath);
+        }
+        else {
+            console.log('Directory does not exist. Try again or type \'exit\'');
+            askingpath();
+        }
+    });
+}
+
+function askingheight(filepath: string){
+    rl.question('Please enter height of the tree: ', (answer)=>{
+        if(isNaN(parseInt(answer))||parseInt(answer)<=1){
+            if(answer=='exit'){
+                rl.close();
+                return;
+            }
+            else{
+                console.log('Wrong value! Try again or type \'exit\'');
+                askingheight(filepath);
+            }
+        }
+        else{
+            const tree = tree_gen(parseInt(answer));
+            fs.writeFileSync(filepath, tree.join('\n'));
+            rl.close();  
+        }
+    })
+}
+
+
+function tree_gen(height:number){
+    console.log('Width of the tree is: ' + (1+(height-1)*4) + ' symbols')
+    let image = []
+
+    //star
+    let firstrow = ''
+    for(let j=(height-1)*2; j>=0; j--){
+            firstrow = firstrow.concat(' ')
+    }
+    firstrow = firstrow.concat('W')
+    image.push(firstrow)
+
+    //main part
+    for(let i=0; i< height; i++){
+        let row = ''
+        let leaves = ''
+        let space = ''
+        for(let j=(height-1)*2-2*i; j>=0; j--){
+            if((i%2!=0)&&(j==0)){
+                space = space.concat('@')
+            }
+            else{
+                space = space.concat(' ')
+            }
+        }
+        row = row.concat(space)
+        for(let k=0; k<(2+(height-1)*2 - space.length)*2 -1 ; k++){
+            leaves = leaves.concat('*')
+        }
+        row = row.concat(leaves)
+        if((i%2==0)&&(i!=0)){
+            row = row.concat('@')
+        }
+        image.push(row)
+    }
+
+    //tree trunc
+    for(let i=0; i<2; i++){
+        let trunc = ''
+        for(let j=(height-1)*2-2; j>=0; j--){
+            trunc = trunc.concat(' ')
+        }
+        trunc = trunc.concat('TTTTT');
+        image.push(trunc)
+    }
+    return image;
+}
+
---
+++ tests/example.spec.ts
@@ -0,0 +1,74 @@
+import { test, expect } from '@playwright/test';
+import { spawn } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const PROJECT_ROOT = path.resolve(__dirname, '..');
+const CLI_PATH = path.resolve(__dirname, '../dist/tree_gen.js');
+
+let createdFiles: string[] = [];
+
+function runCLI(inputs: string[]): Promise<string> {
+  return new Promise((resolve, reject) => {
+    let output = '';
+    const child = spawn('node', [CLI_PATH], {cwd: PROJECT_ROOT});
+
+    child.stdout.on('data', (data) => {
+      output += data.toString();
+    });
+
+    child.stderr.on('data', (data) => {
+      output += data.toString();
+    });
+
+    child.on('error', reject);
+
+    inputs.forEach(input => {
+      child.stdin.write(input + '\n');
+    });
+    child.stdin.end();
+
+    child.on('close', () => resolve(output));
+  });
+}
+
+test('creates tree file', async () => {
+  const filePath = path.join(PROJECT_ROOT, 'test_tree.txt');
+  createdFiles.push(filePath);
+
+  await runCLI(['test_tree.txt', '3']);
+
+  expect(fs.existsSync(filePath)).toBeTruthy();
+});
+
+test('invalid directory', async () => {
+  const invalidpath = path.join(PROJECT_ROOT, 'some_invanid_dir', 'tree.txt');
+  const stdout = await runCLI([invalidpath]);
+
+  expect(stdout).toContain("Directory does not exist");
+
+  expect(fs.existsSync(invalidpath)).toBeFalsy();
+});
+
+test('invalid height', async () => {
+  const invalidvalue = 'abc'
+  const filePath = path.join(PROJECT_ROOT, 'test_tree.txt');
+  createdFiles.push(filePath);
+
+  const stdout = await runCLI(['test_tree.txt', invalidvalue]);
+
+  expect(stdout).toContain("Wrong value");
+  expect(fs.existsSync(filePath)).toBeFalsy();
+});
+
+
+
+test.afterEach(async () => {
+  
+  for (const file of createdFiles) {
+    if (fs.existsSync(file)) {
+      fs.unlinkSync(file);
+    }
+  }
+  createdFiles = [];
+});
